350f196fc794681e3d97baf88cf18128b817ef79
JParamade <114541314+JParamade@users.noreply.github.com>
2025-01-13 04:59:24 +0100

    Defined Vec2 Class Members

---
+++ Practica01/src/Header Files/Vec2.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+class CVec2 {
+public:
+    CVec2();
+    CVec2(float _fX, float _fY);
+    ~CVec2();
+    CVec2(const CVec2& other);
+    
+    CVec2& operator=(const CVec2& other);
+    CVec2& operator+=(const CVec2& other);
+    CVec2& operator-=(const CVec2& other);
+    CVec2& operator*=(const CVec2& other);
+    CVec2& operator/=(const CVec2& other);
+    bool operator==(const CVec2& other);
+    bool operator!=(const CVec2& other);
+    bool operator<(const CVec2& other);
+    bool operator>(const CVec2& other);
+    bool operator<=(const CVec2& other);
+    bool operator>=(const CVec2& other);
+    
+private:
+    float m_fX;
+    float m_fY;
+};
---
+++ Practica01/src/Source Files/Vec2.cpp
@@ -0,0 +1,11 @@
+﻿#include "../src/Header Files/Vec2.h"
+
+CVec2::CVec2()
+    : m_fX(0)
+    , m_fY(0)
+{}
+
+CVec2::CVec2(float _fX, float _fY)
+    : m_fX(_fX)
+    , m_fY(_fY)
+{}
