25482d20c6056d9b8636aa45b9da60a806182adc
JParamade <114541314+JParamade@users.noreply.github.com>
2025-01-19 09:23:37 +0100

    Updated Practice 3

--- Practica03/src/Header Files/CSprite.h
+++ Practica03/src/Header Files/CSprite.h
@@ -6,13 +6,13 @@
 class CSprite {
     typedef void (*CallbackFunc)(CSprite&, float);
 public:
-    CSprite(const ltex_t* _pTexture, int iHFrames = 1, int iVFrames = 1);
+    CSprite(const ltex_t* _pTexture, int _iHFrames = 1, int _iVFrames = 1);
     void SetCallback(CallbackFunc _fFunction);
 
     // Texture Data
-    const ltex_t* GetTexture() const;
     void SetUserData(void* _pData);
-    void SetTexture(const ltex_t* _pTexture, int iHFrames = 1, int iVFrames = 1);
+    const ltex_t* GetTexture() const;
+    void SetTexture(const ltex_t* _pTexture, int _iHFrames = 1, int _iVFrames = 1);
 
     // Blending Mode & Color
     lblend_t GetBlend() const;
@@ -45,4 +45,11 @@ public:
     // Execution Functions
     void Update(float _fDeltaTime);
     void Draw() const;
+private:
+    const ltex_t* m_pTexture;
+    int m_iHFrames;
+    int m_iVFrames;
+    
+    CallbackFunc m_fFunction;
+    void* m_pData;
 };
--- Practica03/src/Source Files/CSprite.cpp
+++ Practica03/src/Source Files/CSprite.cpp
@@ -1,1 +1,30 @@
-﻿#include "CSprite.h"
+﻿#include "../Header Files/CSprite.h"
+
+CSprite::CSprite(const ltex_t* _pTexture, int _iHFrames, int _iVFrames)
+    : m_pTexture(_pTexture)
+    , m_iHFrames(_iHFrames)
+    , m_iVFrames(_iVFrames)
+    , m_fFunction(nullptr)
+    , m_pData(nullptr)
+{}
+
+void CSprite::SetCallback(CallbackFunc _fFunction) {
+    m_fFunction = _fFunction;
+}
+
+void CSprite::SetUserData(void* _pData) {
+    m_pData = _pData;
+}
+
+const ltex_t* CSprite::GetTexture() const {
+    return m_pTexture;
+}
+
+void CSprite::SetTexture(const ltex_t* _pTexture, int _iHFrames, int _iVFrames) {
+    m_pTexture = _pTexture;
+}
+
+
+
+
+
